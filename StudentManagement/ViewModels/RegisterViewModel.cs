8cd79511c94558d2d509449af9e2a25fffa1d312
kailashnadh <mkailashnadhn@gmail.com>
2019-07-23 20:28:55 -0400

    account controller

---
+++ StudentManagement/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("password", ErrorMessage = "password doesnt match")]
+        public string confirmPassword { get; set; }
+    }
+}
